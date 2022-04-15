6fd1ada43b7870c00eefd4e9cdb0922ac991319b
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-15 14:41:04 +0530

    verifed tokens function done

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,11 +2,10 @@ const admin = require('../config/firebase-config')
 
 class Middleware{
     async decodeToken(req,res,next){
-        const token = req.headers.authorization.split(" ");
-        admin.auth().verifyIdToken(token).then(decodedIdToken=>{
-            console.log("Id token decoded", decodedIdToken);
-        })
+        const token = req.headers.authorization.split(' ')[1];
         try{
+            const decodeValue = await admin.auth().verifyIdToken(token);
+            console.log(decodeValue);
             if(decodeValue){
                 return next();
             }
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const PORT = 5000;
 
 app.use(cors());
 
+//global route
 app.use(middleware.decodeToken);
 
 app.get('/api/homescreen',(req,res)=>{
